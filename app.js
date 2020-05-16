22fbed43c773d97ea497784ef296f8e2c7699d6e
wbilalz400 <bilalzahid1997@hotmail.com>
2020-05-17 01:24:54 +0500

    Added Registration and Login capability

--- app.js
+++ app.js
@@ -3,11 +3,51 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose = require('mongoose');
+var passport = require('passport');
+var passportLocal =require('passport-local').Strategy;
+var jwtStrategy = require('passport-jwt').Strategy;
+var jwtExtractor = require('passport-jwt').ExtractJwt;
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var passwordHash = require("password-hash");
+var User = require('./models/user');
 var app = express();
+const connection = mongoose.connect("mongodb://localhost:27017/analytica",{useNewUrlParser: true, useUnifiedTopology: true});
+connection.then( db => {
+  console.log("Connection Success!");
+}, err => console.err(err));
+//Setup Local Strategy of Passport
+passport.use(new passportLocal({
+  usernameField: 'email',
+  passwordField: 'password',
+  },
+  function (email,password,cb) {
+    return User.findOne({'email': email})
+      .then(user => {
+        if (!user) {
+          return cb(null,false, {message: 'Incorrect email or password'});
+        }
+        if (!passwordHash.verify(password,user.password)) {
+          return cb(null,false, {message: 'Incorrect email or password'});
+        }
+
+        return cb (null,user,{message: 'Login Success!'});
+      })
+      .catch(err => cb(err));
+  } 
+));
+
+//Configure JWT Strategy
+passport.use(new jwtStrategy(
+  {jwtFromRequest:jwtExtractor.fromBodyField("jwt"),secretOrKey:"abc123"},
+  function (jwtPayload, cb) {
+    return User.findOne({"_id": jwtPayload._id})
+      .then (user => cb(null,user))
+      .catch (err => cb(err));
+  }
+  ))
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
-
+var User = require('../models/user');
+var passwordHash = require("password-hash");
+var jwt = require("jsonwebtoken");
+var passport = require("passport");
+var mongoose =require('mongoose');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/login',(req,res,next) => {
+  passport.authenticate("local",{session:false}, (err,user,info) => {
+    if (err|| !user) {
+      res.status(400).json({status:"FAIL",data: "An Error Occurred"});
+    }
+    req.login(user,{session:false}, err => {
+      if (err) {
+        res.status(401).json({status:"FAIL",data:err});
+      }
+    });
+    const token = jwt.sign(user.toJSON(),'abc123');
+    return res.json({status: "OK",jwtToken:token,data:"Login Successful!"});
+  })(req,res);
+});
+
+router.post('/register', (req,res,next) => {
+  try {
+    req.body.password = passwordHash.generate(req.body.password);
+    console.log(mongoose.connection.readyState);
+    User.create(req.body)
+    .then(result => res.json({'status':'OK',data:result}),err => res.json({status:"FAIL",data:JSON.stringify(err)}))
+    .catch(error => res.json({status:"FAIL",data:error}));
+  } catch (err) {
+    return res.json({status: "FAIL", data: err});
+  }
+});
 module.exports = router;
